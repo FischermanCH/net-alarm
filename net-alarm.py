4fdf6fae8a0ed3af2004fae3f9369c353323967d
FischermanCH <info@fischerman.ch>
2023-08-06 16:06:29 +0200

    Update: 2023-08-06 16:06:29

--- net-alarm.py
+++ net-alarm.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/arp')
+def arp_page():
+    return render_template('arp_page.html')
+
+@app.route('/tcpip')
+def tcpip_page():
+    return render_template('tcpip_page.html')
+
+@app.route('/host')
+def host_page():
+    return render_template('host_page.html')
+
+@app.route('/lan')
+def lan_page():
+    return render_template('lan_page.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=7777)
