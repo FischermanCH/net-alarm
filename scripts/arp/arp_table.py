f995c2aeddef60fb199c9975f22b4b364e05d42a
FischermanCH <info@fischerman.ch>
2023-08-17 13:54:39 +0200

    Overwrite: 2023-08-17 13:54:39

--- scripts/arp/arp_table.py
+++ scripts/arp/arp_table.py
@@ -12,25 +12,29 @@ def get_latest_arp_file():
 def update_arp_data(latest_file):
     arp_data_path = 'data/arp_data.csv'
     existing_data = []
+    existing_entries = set()  # To store MAC and IP combinations
 
     # Load existing arp_data.csv
     if os.path.exists(arp_data_path):
         with open(arp_data_path, 'r') as f:
             reader = csv.reader(f, delimiter=';')
-            existing_data = list(reader)
+            for row in reader:
+                existing_data.append(row)
+                existing_entries.add((row[0], row[1]))  # MAC and IP combination
 
     # Load latest ARP file
     with open(latest_file, 'r') as f:
         reader = csv.reader(f, delimiter=';')
         new_data = list(reader)
 
-    # Convert timestamp to human-readable date format
+    # Convert timestamp to human-readable date format and check for duplicates
     for row in new_data:
         timestamp = int(row[2])
         formatted_date = datetime.utcfromtimestamp(timestamp).strftime('%d.%m.%y / %H:%M:%S')
         row[2] = formatted_date
-        if row not in existing_data:
+        if (row[0], row[1]) not in existing_entries:  # Check based on MAC and IP
             existing_data.append(row)
+            existing_entries.add((row[0], row[1]))
 
     # Save updated data back to arp_data.csv
     with open(arp_data_path, 'w', newline='') as f:
