c3363e3568e3cbe909cf060ca79ca37424bb18eb
FischermanCH <info@fischerman.ch>
2023-08-24 17:13:13 +0200

    Overwrite: 2023-08-24 17:13:13

--- scripts/arp_arpwatch_log.py
+++ scripts/arp_arpwatch_log.py
@@ -1,14 +1,12 @@
 import os
 
-def get_latest_arpwatch_logfile():
+def get_latest_arpwatch_log():
     log_directory = 'data/arpwatch'
     log_files = [f for f in os.listdir(log_directory) if os.path.isfile(os.path.join(log_directory, f))]
     latest_log_file = max(log_files, key=lambda x: os.path.getmtime(os.path.join(log_directory, x)))
-    return os.path.join(log_directory, latest_log_file)
+    log_file_path = os.path.join(log_directory, latest_log_file)
 
-def get_latest_arpwatch_log():
-    log_file_path = get_latest_arpwatch_logfile()
     with open(log_file_path, 'r') as file:
         log_content = file.readlines()
-    log_content.reverse()  # Reverse the order of the log content
-    return ''.join(log_content)
+
+    return ''.join(reversed(log_content))
